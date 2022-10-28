fa9509d52a4b09fe1383b071fda2c5b358853aec
Bruno Molezzi <bru.molezzi92@gmail.com>
2022-10-28 11:50:19 -0300

    PrimeraEntregaJS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 console.log("link");
->>>>>>> appFirst
 const USER = "demo";
 const PASSWORD = "demo123";
 
@@ -10,20 +7,16 @@ let userUsing = false;
 alert("Bienvenido al constructor de componentes webs!");
 userChoice = parseInt(prompt("Desea usar el constructor o salir del programa? 1)Usar constructor. 2)Salir"));
 
-if(userChoice== 1){
+if(userChoice == 1){
     userUsing = true;
     while(userUsing == true){
         let userLogin = prompt("Ingrese el nombre de usuario").toLowerCase();
         let userPass = prompt("Ingrese la contraseña");
         
         if(userLogin === USER && userPass === PASSWORD){
-<<<<<<< HEAD
-            
-=======
             seleccionBoton();
             alert("Revisa la consola");
             userUsing = false;
->>>>>>> appFirst
         }else{
             alert("Usuario o contraseña incorrecta");
         }
@@ -36,7 +29,3 @@ if(userChoice== 1){
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> appFirst
